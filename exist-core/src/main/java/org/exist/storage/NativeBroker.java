cfc30e32e312b269f480e4046252d0db99defd93
Alan Paxton <alan.paxton@gmail.com>
2025-02-19 13:07:10 +0000

    [optimize] reader uses less granular locking
    Rework EmbeddedXMLStreamReader to hoist locks to the top of its public methods. In particular a big call to `filter()` a large set of noses is much more efficient this way. One sample workload
    went from 61s to 47s.

--- exist-core/src/main/java/org/exist/storage/NativeBroker.java
+++ exist-core/src/main/java/org/exist/storage/NativeBroker.java
@@ -495,7 +495,7 @@ public class NativeBroker extends DBBroker {
     public IEmbeddedXMLStreamReader getXMLStreamReader(final NodeHandle node, final boolean reportAttributes)
             throws IOException, XMLStreamException {
         if(streamReader == null) {
-            final AbstractRawNodeIterator iterator = new GranularRawNodeIterator(this, domDb, node);
+            final ManualLockRawNodeIterator iterator = new ManualLockRawNodeIterator(this, domDb, node);
             streamReader = new EmbeddedXMLStreamReader(this, node.getOwnerDocument(), iterator, node, reportAttributes);
         } else {
             streamReader.reposition(this, node, reportAttributes);
@@ -506,7 +506,7 @@ public class NativeBroker extends DBBroker {
     @Override
     public IEmbeddedXMLStreamReader newXMLStreamReader(final NodeHandle node, final boolean reportAttributes)
             throws IOException, XMLStreamException {
-        final GranularRawNodeIterator iterator = new GranularRawNodeIterator(this, domDb, node);
+        final ManualLockRawNodeIterator iterator = new ManualLockRawNodeIterator(this, domDb, node);
         return new EmbeddedXMLStreamReader(this, node.getOwnerDocument(), iterator, null, reportAttributes);
     }
 
